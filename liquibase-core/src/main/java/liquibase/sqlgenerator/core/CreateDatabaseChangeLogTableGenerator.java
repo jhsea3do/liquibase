7ecbcb32acc5c1fc08cdc9592a5faf7b95a7aa49
Hai Ji <jhsea3do@gmail.com>
2022-03-04 10:31:12 +0800

    CO-5035 add primary key to databasechangelog table
    add composite primary key id,author,filename to databasechangelog table
    see https://docs.liquibase.com/concepts/tracking-tables/databasechangelog-table.html

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/CreateDatabaseChangeLogTableGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/CreateDatabaseChangeLogTableGenerator.java
@@ -31,9 +31,9 @@ public class CreateDatabaseChangeLogTableGenerator extends AbstractSqlGenerator<
         String dateTimeTypeString = getDateTimeTypeString(database);
         CreateTableStatement createTableStatement = new CreateTableStatement(database.getLiquibaseCatalogName(), database.getLiquibaseSchemaName(), database.getDatabaseChangeLogTableName())
                 .setTablespace(database.getLiquibaseTablespaceName())
-                .addColumn("ID", DataTypeFactory.getInstance().fromDescription(charTypeName + "(" + getIdColumnSize() + ")", database), null, null, new NotNullConstraint())
-                .addColumn("AUTHOR", DataTypeFactory.getInstance().fromDescription(charTypeName + "(" + getAuthorColumnSize() + ")", database), null, null, new NotNullConstraint())
-                .addColumn("FILENAME", DataTypeFactory.getInstance().fromDescription(charTypeName + "(" + getFilenameColumnSize() + ")", database), null, null, new NotNullConstraint())
+                .addPrimaryKeyColumn("ID", DataTypeFactory.getInstance().fromDescription(charTypeName + "(" + getIdColumnSize() + ")", database), null, null, null, new NotNullConstraint())
+                .addPrimaryKeyColumn("AUTHOR", DataTypeFactory.getInstance().fromDescription(charTypeName + "(" + getAuthorColumnSize() + ")", database), null, null, null, new NotNullConstraint())
+                .addPrimaryKeyColumn("FILENAME", DataTypeFactory.getInstance().fromDescription(charTypeName + "(" + getFilenameColumnSize() + ")", database), null, null, null, new NotNullConstraint())
                 .addColumn("DATEEXECUTED", DataTypeFactory.getInstance().fromDescription(dateTimeTypeString, database), null, null, new NotNullConstraint())
                 .addColumn("ORDEREXECUTED", DataTypeFactory.getInstance().fromDescription("int", database), null, null, new NotNullConstraint())
                 .addColumn("EXECTYPE", DataTypeFactory.getInstance().fromDescription(charTypeName + "(10)", database), null, null, new NotNullConstraint())
